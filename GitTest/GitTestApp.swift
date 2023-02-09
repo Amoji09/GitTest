651e3c311a1e9a5b056cb338e1904e022edf9f81
Amogh Mantri <amogh.mantri@yahoo.com>
2023-02-09 18:37:35 -0500

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Amogh Mantri on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
